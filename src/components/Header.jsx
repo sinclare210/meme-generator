d146838a0dbf32b7c816fa5b5f6330b0c1776a6e
Sinclair <sinclairolajuwon@gmail.com>
2024-09-25 15:48:17 -0500

    meme-generator done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex items-center justify-center bg-gradient-to-r from-purps-0 to-purpf-0 shadow-lg'>
-        <img src="public/troll-face.png" alt="" className='w-16 p-2'/>
+        <img src="/troll-face.png" alt="" className='w-16 p-2'/>
         <h1 className='text-3xl font-bold p-4 text-white font-poppins'>Sinclair's Meme Generator</h1>
     </div>
   )
